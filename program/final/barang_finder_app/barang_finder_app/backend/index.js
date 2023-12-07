6404837ea1dec264acf5c68d87689d3a3b37923b
azmialdi67 <acogan52@gmail.com>
2023-12-07 17:58:09 +0700

    add backend broow

---
+++ program/final/barang_finder_app/barang_finder_app/backend/index.js
@@ -0,0 +1,22 @@
+// index.js
+const express = require('express');
+const app = express();
+const db = require('./koneksi/koneksi');
+const rute = require('./rute/rute');  // Import file rute
+
+const PORT = process.env.PORT || 4000;
+
+// Middleware untuk menghandle body JSON pada request
+app.use(express.json());
+
+// Gunakan rute yang telah di-import
+app.use('/', rute);
+
+app.use((req, res) => {
+  res.status(404).json({ error: 'Endpoint kaga ada nape lu hah?!' });
+});
+
+// Menjalankan server
+app.listen(PORT, () => {
+  console.log(`Server berjalan di port ${PORT}`);
+});
---
+++ program/final/barang_finder_app/barang_finder_app/backend/rute/rute.js
@@ -0,0 +1,70 @@
+// routes.js
+const express = require('express');
+const rute = express.Router();
+const db = require('../koneksi/koneksi');
+
+// Endpoint untuk mendapatkan data dari tabel 'like'
+rute.get('/likes', (req, res) => {
+  db.query('SELECT * FROM `like`', (error, results, fields) => {
+    if (error) {
+      console.error('Ada masalah bray pas buka tabel "like" :', error);
+      return res.status(500).json({ error: 'Ada masalah bray makanya ga bisa buka tabelnya :(' });
+    }
+
+    if (results.length === 0) {
+      return res.status(404).json({ message: 'Tabel "like" kosong' });
+    }
+
+    res.json(results);
+  });
+});
+
+// Endpoint untuk mendapatkan data dari tabel 'post'
+rute.get('/posts', (req, res) => {
+  db.query('SELECT * FROM post', (error, results, fields) => {
+    if (error) {
+      console.error('Ada masalah bray pas buka tabel "post" :', error);
+      return res.status(500).json({ error: 'Ada masalah bray makanya ga bisa buka tabelnya :(' });
+    }
+
+    if (results.length === 0) {
+      return res.status(404).json({ message: 'Tabel "Post" kosong' });
+    }
+
+    res.json(results);
+  });
+});
+
+// Endpoint untuk mendapatkan data dari tabel 'product_post'
+rute.get('/product_posts', (req, res) => {
+  db.query('SELECT * FROM product_post', (error, results, fields) => {
+    if (error) {
+      console.error('Ada masalah bray pas buka product_tabel "post" :', error);
+      return res.status(500).json({ error: 'Ada masalah bray makanya ga bisa buka tabelnya :(' });
+    }
+
+    if (results.length === 0) {
+      return res.status(404).json({ message: 'Tabel "product_post" kosong' });
+    }
+
+    res.json(results);
+  });
+});
+
+// Endpoint untuk mendapatkan data dari tabel 'user'
+rute.get('/users', (req, res) => {
+  db.query('SELECT * FROM user', (error, results, fields) => {
+    if (error) {
+      console.error('Ada masalah bray pas buka tabel "user" :', error);
+      return res.status(500).json({ error: 'Ada masalah bray makanya ga bisa buka tabelnya :(' });
+    }
+
+    if (results.length === 0) {
+      return res.status(404).json({ message: 'Tabel "user" kosong' });
+    }
+
+    res.json(results);
+  });
+});
+
+module.exports = rute;
